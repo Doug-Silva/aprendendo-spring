471c7f1d660d7a39d53e022fd5f13218a81bd82e
Doug-Silva <silvatec.br@gmail.com>
2026-05-27 20:38:09 -0300

    Criando uma Service

---
+++ src/main/java/com/javanauta/aprendendo_spring/business/UsuarioService.java
@@ -0,0 +1,38 @@
+package com.javanauta.aprendendo_spring.business;
+
+import com.javanauta.aprendendo_spring.infrastructure.entity.Usuario;
+import com.javanauta.aprendendo_spring.infrastructure.exceptions.ConflictException;
+import com.javanauta.aprendendo_spring.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public Usuario salvaUsuario(Usuario usuario) {
+        try {
+            emailExiste(usuario.getEmail());
+            return usuarioRepository.save(usuario);
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado", e.getCause());
+        }
+    }
+
+    public void emailExiste(String email){
+        try {
+            boolean existe = verificaEmailExistente(email);
+            if (existe) {
+                throw new ConflictException("Email já cadastrado" + email);
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado", e.getCause());
+        }
+    }
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+}
